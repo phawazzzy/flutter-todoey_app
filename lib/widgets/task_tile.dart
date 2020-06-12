1b1da8453169fdbea4e3253c511170fb345ff1fb
kareem fawas <phawazzzy@gmail.com>
2020-06-12 01:52:07 +0100

    added task to be cancelled when checked4

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,31 +1,40 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
-  const TaskTile({
-    Key key,
-  }) : super(key: key);
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+  void callBack(bool newValue) {
+    setState(() {
+      isChecked = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('this  is a task'),
-      trailing: CheckboxTile(),
+      title: Text('this  is a task', style: TextStyle(decoration: isChecked ? TextDecoration.lineThrough : null),),
+      trailing: CheckboxTile(isChecked, callBack),
     );
   }
 }
 
-class CheckboxTile extends StatefulWidget {
-  @override
-  _CheckboxTileState createState() => _CheckboxTileState();
-}
 
-class _CheckboxTileState extends State<CheckboxTile> {
-  bool isChecked = false;
+class CheckboxTile extends StatelessWidget {
+  final bool checkboxState;
+  final Function isCheckedCallBack;
+  CheckboxTile(this.checkboxState, this.isCheckedCallBack);
   @override
   Widget build(BuildContext context) {
     return Checkbox(
         activeColor: Colors.lightBlueAccent,
-        value: isChecked,
-        onChanged: (newValue) => setState(() => isChecked = newValue));
+        value: checkboxState,
+        onChanged: isCheckedCallBack
+    );
   }
 }
